ab5997daa295d0a6a75946eeef9b081d46c11b2c
Mohitb07 <bmohit980@gmail.com>
2023-10-12 20:27:38 +0530

    feat: utility methods added

---
+++ lib/cloudinaryImage.ts
@@ -0,0 +1,21 @@
+import { CloudinaryImage } from "@cloudinary/url-gen";
+import { fit } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+
+export const cloudinaryImage = ({
+  imageUrl,
+  height,
+  width,
+}: {
+  imageUrl: string;
+  height: number;
+  width: number;
+}) => {
+  const image = new CloudinaryImage(imageUrl, {
+    cloudName: process.env.CLOUDINARY_NAME,
+  })
+    .resize(fit().width(width).height(height))
+    .roundCorners(byRadius(20))
+    .format("png");
+  return image;
+};

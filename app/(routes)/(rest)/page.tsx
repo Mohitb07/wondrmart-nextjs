630dec0cc186a468ef955e65292409cb11df16ec
Mohitb07 <bmohit980@gmail.com>
2023-10-12 20:28:48 +0530

    refactor: code refactored

--- app/(routes)/(rest)/page.tsx
+++ app/(routes)/(rest)/page.tsx
@@ -1,17 +1,15 @@
 import Container from "@/common/Container";
 import Search from "@/common/Search";
+
 import ProductsList from "./components/ProductsList";
 
 export default async function Home() {
-  console.log("home");
   return (
     <Container>
-      <main className="p-6">
-        <div className="space-y-5">
-          <h1 className="text-4xl font-bold">Products</h1>
-          <Search />
-          <ProductsList/>
-        </div>
+      <main className="p-6 space-y-5">
+        <h1 className="text-4xl font-bold">Products</h1>
+        <Search />
+        <ProductsList />
       </main>
     </Container>
   );

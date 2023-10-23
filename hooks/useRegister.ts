468ce9493b72339916ea42964351fc7766aec8c1
Mohitb07 <bmohit980@gmail.com>
2023-10-23 20:34:28 +0530

    feat: custom user register hook added

---
+++ hooks/useRegister.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+import { createUser } from "@/actions/createUser";
+import { SignUpFormData, UserData } from "@/types";
+
+type Error = AxiosError<
+  {
+    message: {
+      [key in keyof SignUpFormData]: string;
+    };
+  },
+  any
+>;
+
+const useRegister = () => {
+  const queryClient = useQueryClient();
+  return useMutation<UserData, Error, SignUpFormData, UserData>({
+    mutationFn: (data: SignUpFormData) =>
+      createUser({
+        email: data.email,
+        address: data.address,
+        confirmPassword: data.confirmPassword,
+        password: data.password,
+        phone: data.phone,
+        username: data.username,
+      }),
+    onSuccess: (data: UserData) => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      console.log('data', data)
+    //   window.location.href = "/";
+    },
+  });
+};
+
+export default useRegister;

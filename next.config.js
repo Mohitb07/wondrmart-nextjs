f451210dd330e65291c79e9d9ec5ff53a7f60c60
Mohitb07 <bmohit980@gmail.com>
2024-01-01 21:22:21 +0530

    Add configuration for redirects, images, and environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/auth/signin",
+        permanent: true,
+      },
+      {
+        source: "/register",
+        destination: "/auth/signup",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

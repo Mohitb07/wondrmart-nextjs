adab5cf7343bddd0197705d623ca7b1d9231b4c3
Mohitb07 <bmohit980@gmail.com>
2024-01-01 21:23:19 +0530

    Add CardCTA component for product CTA

---
+++ app/(routes)/(rest)/product/[productId]/components/CTA/Card/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  Button,
+  Divider,
+} from "@nextui-org/react";
+import ProductServices from "../../Services";
+
+
+type CardCTAProps = {};
+
+const CardCTA: React.FC<CardCTAProps> = () => {
+  return (
+    <Card className="py-4">
+      <CardHeader className="pb-0 pt-2 px-4 flex items-start gap-7 justify-center">
+        <ProductServices/>
+      </CardHeader>
+      <Divider className="my-1"/>
+      <CardBody className="overflow-visible py-2">
+        <div className="gap-3 flex flex-col">
+          <Button color="primary" variant="solid" fullWidth>
+            Add to Cart
+          </Button>
+          <Button color="primary" variant="bordered" fullWidth>
+            Buy Now
+          </Button>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
+export default CardCTA;

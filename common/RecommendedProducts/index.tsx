7e12bec148384ba9da88e2861fde784174f89123
Mohitb07 <bmohit980@gmail.com>
2024-01-05 21:54:10 +0530

    Add RecommendedProducts component

---
+++ common/RecommendedProducts/index.tsx
@@ -0,0 +1,142 @@
+import ProductCard from "@/app/(routes)/(rest)/components/ProductCard";
+import ProductsList from "@/app/(routes)/(rest)/components/ProductsList";
+import React from "react";
+
+type RecommendedProductsProps = {};
+
+const RecommendedProducts: React.FC<RecommendedProductsProps> = () => {
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl">You may like</h2>
+      <div className="flex items-center overflow-x-scroll gap-5">
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se fadffffffffffffffffffffffffffffffff"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+        <ProductCard
+          id="123"
+          image_url="fasfd"
+          isInCartLoading={false}
+          name="watch se"
+          price="200"
+        />
+      </div>
+    </div>
+  );
+};
+export default RecommendedProducts;

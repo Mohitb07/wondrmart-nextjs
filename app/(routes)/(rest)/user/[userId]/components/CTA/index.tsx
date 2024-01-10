a9cd7a91bca665964a73a6311069bca2acf1cd4e
Mohitb07 <bmohit980@gmail.com>
2024-01-10 21:54:14 +0530

    refactor: cta component to include navigation functionality

--- app/(routes)/(rest)/user/[userId]/components/CTA/index.tsx
+++ app/(routes)/(rest)/user/[userId]/components/CTA/index.tsx
@@ -1,27 +1,40 @@
 "use client";
 
 import { Card, CardBody } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import React from "react";
-import { IconType } from "react-icons";
-
 
 type AccountCTAProps = {
-    Logo: React.ReactNode;
-    title: string;
-    subtitle: string;
+  Logo: React.ReactNode;
+  title: string;
+  subtitle: string;
+  userId: string;
+  navigateTo?: string;
 };
 
-const AccountCTA: React.FC<AccountCTAProps> = ({Logo, title = '', subtitle = ''}) => {
+const AccountCTA: React.FC<AccountCTAProps> = ({
+  Logo,
+  title = "",
+  subtitle = "",
+  userId,
+  navigateTo = "",
+}) => {
+  const router = useRouter();
+
+  const navigationHandler = () => {
+    router.push(`/user/${userId}/${navigateTo}`);
+  };
+
   return (
-    <Card isPressable>
+    <Card isPressable onClick={navigationHandler}>
       <CardBody>
         <div className="flex flex-col items-center gap-3 md:flex-row">
-          <div>
-            {Logo}
-          </div>
+          <div>{Logo}</div>
           <div className="leading-7 text-center lg:text-left">
             <h2 className="text-lg lg:text-xl">{title}</h2>
-            <span className="text-base lg:text-lg text-slate-400">{subtitle}</span>
+            <span className="text-base lg:text-lg text-slate-400">
+              {subtitle}
+            </span>
           </div>
         </div>
       </CardBody>

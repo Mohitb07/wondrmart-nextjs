4eab9c687829a63f24d12578db2da63db0d853cf
Mohitb07 <bmohit980@gmail.com>
2024-01-11 20:37:33 +0530

    refactor: user address module

--- app/(routes)/(rest)/user/[userId]/address/components/Card.tsx
+++ app/(routes)/(rest)/user/[userId]/address/components/Card.tsx
@@ -7,31 +7,69 @@ import {
   CardHeader,
   Divider,
 } from "@nextui-org/react";
-import Image from "next/image";
+
+import Link from "next/link";
 import React from "react";
 
-type CardProps = {};
+type CardProps = {
+  userId?: string;
+  isDefault?: boolean;
+  children?: React.ReactNode;
+};
 
-const StyledCard: React.FC<CardProps> = () => {
+const StyledCard: React.FC<CardProps> = ({
+  userId,
+  isDefault = false,
+  children,
+}) => {
   return (
-    <Card className="w-full md:min-w-[300px] md:max-w-[380px]">
-      <CardHeader className="flex gap-3 text-xs p-3 px-6">
-        <p>Default: </p>
-        <span>wondrMart</span>
-      </CardHeader>
-      <Divider />
-      <CardBody className="text-sm p-3 px-6">
-        <span>Testing </span>
-        <span>T-T TTT TTT</span>
-        <span>Testing Location</span>
-        <span>Testing, Testing 1100123</span>
-        <span>India</span>
-        <span>Phone number: 123456789</span>
+    <Card
+      className={`w-full md:min-w-[300px] md:max-w-[380px] min-h-[240px] border border-2 ${
+        isDefault ? "border-primary" : "border-transparent"
+      }`}
+    >
+      {isDefault && (
+        <>
+          <CardHeader className="flex gap-3 text-xs p-3 px-6">
+            <p>Default: </p>
+            <span>wondrMart</span>
+          </CardHeader>
+          <Divider />
+        </>
+      )}
+      <CardBody className="text-sm p-3 px-6 flex justify-center">
+        {children || (
+          <>
+            <span>Testing </span>
+            <span>T-T TTT TTT</span>
+            <span>Testing Location</span>
+            <span>Testing, Testing 1100123</span>
+            <span>India</span>
+            <span>Phone number: 123456789</span>
+          </>
+        )}
       </CardBody>
       <CardFooter className="flex text-sm gap-3 px-6">
-        <div>Edit</div>
-        <Divider orientation="vertical" />
-        <div>Remove</div>
+        {!children && (
+          <>
+            <div>
+              <Link
+                href="/user/[userId]/address/[mode]"
+                as={`/user/${userId}/address/edit`}
+              >
+                Edit
+              </Link>
+            </div>
+            <Divider orientation="vertical" />
+            <div>Remove</div>
+            {!isDefault && (
+              <>
+                <Divider orientation="vertical" />
+                <div>Set as Default</div>
+              </>
+            )}
+          </>
+        )}
       </CardFooter>
     </Card>
   );

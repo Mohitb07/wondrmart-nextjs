a1edac09d27cf0c25ea232556d898392a6f1fd3c
Mohitb07 <bmohit980@gmail.com>
2024-01-30 21:38:00 +0530

    feat: bread crumb component for user address page

---
+++ app/(routes)/(rest)/user/[userId]/address/[mode]/components/BreadCrumb.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+type BreadCrumbProps = {
+  mode: string;
+};
+
+const BreadCrumb: React.FC<BreadCrumbProps> = ({ mode }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+
+  return (
+    <Breadcrumbs size="lg">
+      <BreadcrumbItem onClick={handleClick}>Back</BreadcrumbItem>
+      <BreadcrumbItem>{mode === "create" ? "Add" : "Edit"}</BreadcrumbItem>
+    </Breadcrumbs>
+  );
+};
+export default BreadCrumb;

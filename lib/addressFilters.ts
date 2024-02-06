f836fc4922858942bbd231bcb47e4633c0d3f0a2
Mohitb07 <bmohit980@gmail.com>
2024-02-06 11:28:22 +0530

    refactor: folder renamed to utils

--- lib/addressFilters.ts
+++
@@ -1,9 +0,0 @@
-export const isPinCodeValid = (pinCode: string): boolean => {
-  const pinCodeRegex = /^[0-9]{1,6}$/;
-  return pinCodeRegex.test(pinCode);
-};
-
-export const isMobileNumberValid = (mobileNumber: string): boolean => {
-  const mobileNumberRegex = /^[0-9]{1,10}$/;
-  return mobileNumberRegex.test(mobileNumber);
-};
--- lib/debounce.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (func: Function, delay: number) => {
-  let timer: ReturnType<typeof setTimeout>;
-  return function (...args: any) {
-    clearTimeout(timer);
-    timer = setTimeout(() => func(...args), delay);
-  };
-};

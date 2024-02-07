93fed0f8e71583bbc89b11d09baf72b66bb72a30
Mohitb07 <bmohit980@gmail.com>
2024-02-07 21:55:07 +0530

    Add optional header and footer visibility props to Card component

--- app/(routes)/(rest)/user/[userId]/addresses/components/Card.tsx
+++ app/(routes)/(rest)/user/[userId]/addresses/components/Card.tsx
@@ -15,20 +15,24 @@ type CardProps = {
   userId?: string;
   isDefault?: boolean;
   children?: React.ReactNode;
+  isHeaderVisible?: boolean;
+  isFooterVisible?: boolean;
 };
 
 const StyledCard: React.FC<CardProps> = ({
   userId,
   isDefault = false,
   children,
+  isHeaderVisible = true,
+  isFooterVisible = true,
 }) => {
   return (
     <Card
-      className={`w-full md:min-w-[300px] md:max-w-[380px] min-h-[240px] border border-2 ${
+      className={`w-full md:min-w-[300px] md:max-w-[380px] min-h-[240px] border-2 ${
         isDefault ? "border-primary" : "border-transparent"
       }`}
     >
-      {isDefault && (
+      {isHeaderVisible && isDefault && (
         <>
           <CardHeader className="flex gap-3 text-xs p-3 px-6">
             <p>Default: </p>
@@ -39,18 +43,18 @@ const StyledCard: React.FC<CardProps> = ({
       )}
       <CardBody className="text-sm p-3 px-6 flex justify-center">
         {children || (
-          <>
+          <div className="leading-8 flex flex-col">
             <span>Testing </span>
             <span>T-T TTT TTT</span>
             <span>Testing Location</span>
             <span>Testing, Testing 1100123</span>
             <span>India</span>
             <span>Phone number: 123456789</span>
-          </>
+          </div>
         )}
       </CardBody>
       <CardFooter className="flex text-sm gap-3 px-6">
-        {!children && (
+        {isFooterVisible &&!children && (
           <>
             <div>
               <Link

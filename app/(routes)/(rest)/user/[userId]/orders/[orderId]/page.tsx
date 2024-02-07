9a3a7ae3016bd9d8d5e5f9952e06baff0df214a3
Mohitb07 <bmohit980@gmail.com>
2024-02-07 21:52:19 +0530

    feat: now users can see their order detail

---
+++ app/(routes)/(rest)/user/[userId]/orders/[orderId]/page.tsx
@@ -0,0 +1,43 @@
+import CartItem from "@/app/(routes)/(rest)/cart/components/CartItem";
+import StyledCard from "../../addresses/components/Card";
+import OrderCard from "../components/OrderCard";
+import OrderSummary from "@/app/(routes)/(rest)/cart/components/Summary";
+
+export const metadata = {
+  title: "Order Detail",
+  description: "Order Detail",
+};
+
+type OrderDetailProps = {
+  params: {
+    orderId: string;
+  };
+};
+
+export default function OrderDetail({ params }: OrderDetailProps) {
+  return (
+    <div className="">
+      <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-10 w-full ">
+        <div className="space-y-3  h-full">
+          <h2 className="text-xl font-bold">Shipped To</h2>
+          <StyledCard isFooterVisible={false} isHeaderVisible={false} />
+        </div>
+        <div className="space-y-3 h-full">
+          <h2 className="text-xl font-bold">Order Summary</h2>
+          <OrderSummary textSize="sm" amountSize="base"/>
+        </div>
+      </div>
+      <div className="mt-5 space-y-5">
+        <OrderCard orderId="313" isOrderDetail />
+        <OrderCard orderId="313312" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+        <OrderCard orderId="31351" isOrderDetail />
+      </div>
+    </div>
+  );
+}

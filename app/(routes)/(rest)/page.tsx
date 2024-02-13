ce7e87a631ecb0a469fabac3d16cbd1ec55f817c
Mohitb07 <bmohit980@gmail.com>
2024-02-13 21:30:28 +0530

    Add server-side rendering with React Query hydration

--- app/(routes)/(rest)/page.tsx
+++ app/(routes)/(rest)/page.tsx
@@ -1,16 +1,28 @@
 import Container from "@/common/Container";
 import Search from "@/common/Search";
+import { dehydrate, Hydrate } from "@tanstack/react-query";
 
 import ProductsList from "./components/ProductsList";
+import getQueryClient from "./components/getQueryClient";
+import { getAllProducts } from "@/actions/getProducts";
 
 export default async function Home() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["products", ""],
+    queryFn: () => getAllProducts(""),
+  });
+  const dehydrateState = dehydrate(queryClient);
+
   return (
-    <Container>
-      <main className="p-6 space-y-5">
-        <h1 className="text-4xl font-bold">Products</h1>
-        <Search />
-        <ProductsList />
-      </main>
-    </Container>
+    <Hydrate state={dehydrateState}>
+      <Container>
+        <main className="p-6 space-y-5">
+          <h1 className="text-4xl font-bold">Products</h1>
+          <Search />
+          <ProductsList />
+        </main>
+      </Container>
+    </Hydrate>
   );
 }

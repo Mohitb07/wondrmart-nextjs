9e1f04c7173b7657c6eaaa1f8ed8cdfe883c3201
Mohitb07 <bmohit980@gmail.com>
2024-02-13 21:35:05 +0530

    Update import of Cookies library in useGetUser hook

--- hooks/useGetUser.ts
+++ hooks/useGetUser.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import * as Cookies from "tiny-cookie";
+import Cookies from "js-cookie";
 
 import { getUser } from "@/actions/getUser";
 import { User } from "../types";
@@ -8,7 +8,7 @@ import { User } from "../types";
 type Error = AxiosError;
 
 const useGetUser = () => {
-  const token = Cookies.getCookie("accessToken");
+  const token = Cookies.get("accessToken");
   return useQuery<User, Error>({
     queryKey: ["user"],
     queryFn: getUser,

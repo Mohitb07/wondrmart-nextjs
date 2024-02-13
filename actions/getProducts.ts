cf2ad73f010d19046f6d581f86fd134c43523dc7
Mohitb07 <bmohit980@gmail.com>
2024-02-13 21:28:52 +0530

    Refactor getProducts function to getAllProducts and update API call

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -1,5 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/api";
+import { Product } from "@/types";
 
-export const getProducts = async (query: string) => {
-  
+// export const getProducts = async (query: string) => {};
+
+type Filters = string;
+export const getAllProducts = async (filters: Filters): Promise<Product[]> => {
+  const res = await axiosInstance.post("/products", {
+    query: filters,
+  });
+  return res.data;
 };

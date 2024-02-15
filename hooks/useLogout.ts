eb0f98e6fe68bf555dbaf06cc9335ca7d1751579
Mohitb07 <bmohit980@gmail.com>
2024-02-15 22:13:42 +0530

    update to use js-cookie instead of tiny-cookie

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,10 +1,10 @@
 import { useQueryClient } from "@tanstack/react-query";
-import * as Cookies from "tiny-cookie";
+import Cookies from "js-cookie";
 
 export const useLogOut = () => {
   const queryClient = useQueryClient();
   const logOut = () => {
-    Cookies.removeCookie("accessToken");
+    Cookies.remove("accessToken");
     queryClient.removeQueries({ queryKey: ["user"], exact: true });
     queryClient.removeQueries({ queryKey: ["cartItems"], exact: true });
     window.location.href = "/";

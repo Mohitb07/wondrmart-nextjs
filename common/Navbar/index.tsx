982fdecc74a833b0d07381a5423ac414c118cb26
Mohitb07 <bmohit980@gmail.com>
2024-02-21 21:04:44 +0530

    refactor: authentication logic

--- common/Navbar/index.tsx
+++ common/Navbar/index.tsx
@@ -29,7 +29,7 @@ import { AcmeLogo } from "./Logo";
 export default function StyledNavbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const { logOut } = useLogOut();
-  const { data: user, isInitialLoading } = useGetUser();
+  const { data: user, status } = useGetUser();
   const pathname = usePathname();
 
   const menuItems = [
@@ -46,83 +46,6 @@ export default function StyledNavbar() {
       href: "/topseller",
     },
   ];
-
-  let authStateContent = null;
-
-  if (isInitialLoading) {
-    authStateContent = (
-      <NavbarContent justify="end">
-        <Spinner />
-      </NavbarContent>
-    );
-  } else if (user) {
-    authStateContent = (
-      <NavbarContent as="div" justify="end">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="primary"
-              name={user.username}
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">{user.email}</p>
-            </DropdownItem>
-            <DropdownItem key="profile">
-              <Link className="w-full" color="foreground" href="/user/123">
-                Profile
-              </Link>
-            </DropdownItem>
-            <DropdownItem key="cart">
-              <Link className="w-full" color="foreground" href="/cart">
-                Cart
-              </Link>
-            </DropdownItem>
-            <DropdownItem key="orders">
-              <Link className="w-full" color="foreground" href="/orders">
-                Orders
-              </Link>
-            </DropdownItem>
-            <DropdownItem
-              className="w-full"
-              onClick={logOut}
-              key="logout"
-              color="danger"
-            >
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-    );
-  } else {
-    authStateContent = (
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link as={NextLink} href="/auth/signin">
-            Login
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button
-            as={NextLink}
-            color="primary"
-            href="/auth/signup"
-            variant="shadow"
-          >
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    );
-  }
 
   return (
     <Navbar
@@ -158,7 +81,84 @@ export default function StyledNavbar() {
           </NavbarItem>
         ))}
       </NavbarContent>
-      {authStateContent}
+      {status === "loading" ? (
+        <NavbarContent justify="end">
+          <Spinner color="primary" />
+        </NavbarContent>
+      ) : status === "success" ? (
+        <NavbarContent as="div" justify="end">
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="primary"
+                name={user.username}
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{user.email}</p>
+              </DropdownItem>
+              <DropdownItem key="profile">
+                <Link
+                  as={NextLink}
+                  className="w-full"
+                  color="foreground"
+                  href="/user/123"
+                >
+                  Profile
+                </Link>
+              </DropdownItem>
+              <DropdownItem key="cart">
+                <Link
+                  as={NextLink}
+                  className="w-full"
+                  color="foreground"
+                  href="/cart"
+                >
+                  Cart
+                </Link>
+              </DropdownItem>
+              <DropdownItem key="orders">
+                <Link className="w-full" color="foreground" href="/orders">
+                  Orders
+                </Link>
+              </DropdownItem>
+              <DropdownItem
+                className="w-full"
+                onClick={logOut}
+                key="logout"
+                color="danger"
+              >
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      ) : (
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden lg:flex">
+            <Link as={NextLink} href="/auth/signin">
+              Login
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              as={NextLink}
+              color="primary"
+              href="/auth/signup"
+              variant="shadow"
+            >
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      )}
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item.name}-${index}`}>

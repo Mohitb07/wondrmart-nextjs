727e5727c49c593c1784f004868d3d16d479f9d8
Mohitb07 <bmohit980@gmail.com>
2024-02-21 21:04:03 +0530

    Add cart functionality to ProductsList component

--- app/(routes)/(rest)/components/ProductsList.tsx
+++ app/(routes)/(rest)/components/ProductsList.tsx
@@ -6,25 +6,47 @@ import ProductCard from "./ProductCard";
 import { useQuery } from "@tanstack/react-query";
 import { getAllProducts } from "@/actions/getProducts";
 import { Spinner } from "@nextui-org/react";
+import { getCartItems } from "@/actions/getCartItems";
+import useGetUser from "@/hooks/useGetUser";
 
 export default function ProductsList() {
   const params = useSearchParams();
+  const { data: user, isLoading: isUserLoading } = useGetUser();
   const { data, isLoading, error } = useQuery({
     queryKey: ["products", params.get("q") || ""],
     queryFn: () => getAllProducts(params.get("q") || ""),
   });
-  console.log("products list", data, isLoading, error);
+  const {
+    data: cartItems,
+    isInitialLoading: isCartItemsLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["cartItems"],
+    queryFn: getCartItems,
+    enabled: !!user && !isUserLoading,
+    onError: (err) => {
+      console.error("Error fetching cart items", err);
+    },
+  });
 
   if (isLoading) {
     return (
       <div className="flex items-center justify-center h-[500px] ">
-        <Spinner color="primary"/>
+        <Spinner color="primary" />
       </div>
     );
   }
   if (error) {
     return <div>Error: {JSON.stringify(error)}</div>;
   }
+
+  let cartItemsIds: Record<string, string> = {};
+  if (!isError && cartItems && cartItems.cart.length) {
+    cartItems.cart[0].cart_items.map((item) => {
+      cartItemsIds[item.product_id] = item.quantity.toString();
+    });
+  }
+
   return (
     <Container>
       {/* <h1>Product list {params.get("q")}</h1> */}
@@ -34,9 +56,12 @@ export default function ProductsList() {
             key={product.product_id}
             id={product.product_id}
             image_url={product.image_url}
-            isInCartLoading={false}
+            isInCartLoading={isCartItemsLoading}
             name={product.name}
             price={product.price}
+            productId={product.product_id}
+            cartQty={cartItemsIds[product.product_id] || "0"}
+            cartId={cartItems?.cart[0]?.cart_id}
           />
         ))}
       </div>

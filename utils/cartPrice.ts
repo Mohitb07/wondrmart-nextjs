712068a938a12bbce83182cb7513775e89d48df8
Mohitb07 <bmohit980@gmail.com>
2024-02-21 21:06:26 +0530

    Add calculateCartPrice function to calculate the total price of items in the cart

---
+++ utils/cartPrice.ts
@@ -0,0 +1,10 @@
+import { CartItem } from "@/types";
+
+export const calculateCartPrice = (cartItems: CartItem[]) => {
+  return cartItems.reduce(
+    (acc: number, currentCartItem: CartItem) =>
+      acc +
+      Number(currentCartItem.quantity) * Number(currentCartItem.products.price),
+    0
+  );
+};

a331998ac522b2a12ec491dd89e5387d0eb2c7e3
Mohitb07 <bmohit980@gmail.com>
2024-02-26 21:54:30 +0530

    refactor: function to use axios directly

--- actions/getProduct.ts
+++ actions/getProduct.ts
@@ -1,9 +1,10 @@
-import { axiosInstance } from "@/api";
+import { BASE_URL } from "@/api";
 import { Product } from "@/types";
+import axios from "axios";
 
 export const getProduct = async (productId: string): Promise<Product> => {
   try {
-    const res = await axiosInstance.get(`/products/${productId}`);
+    const res = await axios.get(BASE_URL + `/products/${productId}`)
     return res.data;
   } catch (error) {
     throw error;

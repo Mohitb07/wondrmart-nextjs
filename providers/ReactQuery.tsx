7854c7fa674e2eb2d4a156c2e13771aad50aa579
Mohitb07 <bmohit980@gmail.com>
2024-03-04 21:02:24 +0530

    refactor: to use useState for creating the QueryClient

--- providers/ReactQuery.tsx
+++ providers/ReactQuery.tsx
@@ -1,15 +1,18 @@
 // app/providers.tsx
 "use client";
 
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export function ReactQueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
+  const [client] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
   );
 }

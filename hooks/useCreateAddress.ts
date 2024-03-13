ce5b45fdb5d3580b1221f8b09327be902ac009b4
Mohitb07 <bmohit980@gmail.com>
2024-03-13 20:48:00 +0530

    Add address-related hooks

---
+++ hooks/useCreateAddress.ts
@@ -0,0 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { createAddress } from "@/actions/createAddress";
+import { AddressFormData } from "@/types";
+import { usePathname } from "next/navigation";
+
+const useCreateAddress = () => {
+  const pathname = usePathname();
+  const paths = pathname.split("/").slice(0, -1);
+  const url = paths.join("/");
+  return useMutation({
+    mutationFn: (data: AddressFormData) => {
+      return createAddress({
+        apartment: data.apartment,
+        area: data.area,
+        city: data.city,
+        state: data.state,
+        country: data.country,
+        isDefault: data.isDefault,
+        mobile: data.mobile,
+        name: data.name,
+        pinCode: data.pinCode,
+      });
+    },
+    onSuccess: (data, variables) => {
+      window.location.href = url;
+    },
+    onError: (error, variables, context) => {},
+  });
+};
+
+export default useCreateAddress;

558d75b1d03c2f4eb01b26a3107dc48f787bb2af
Mohitb07 <bmohit980@gmail.com>
2024-03-21 21:40:00 +0530

    refactor: user loading skeleton

--- common/Navbar/index.tsx
+++ common/Navbar/index.tsx
@@ -84,14 +84,7 @@ export default function StyledNavbar() {
       </NavbarContent>
       {status === "loading" ? (
         <NavbarContent justify="end">
-          <div className="max-w-[150px] w-full flex items-center gap-3">
-            <div>
-              <Skeleton className="flex rounded-full w-12 h-12" />
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <Skeleton className="h-5 w-3/5 rounded-lg" />
-            </div>
-          </div>
+          <Skeleton className="flex rounded-full w-10 h-10" />
         </NavbarContent>
       ) : status === "success" ? (
         <NavbarContent as="div" justify="end">

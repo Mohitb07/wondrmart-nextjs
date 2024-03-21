2998dff4af6994061b2bb7668531e3fdda4fa245
Mohitb07 <bmohit980@gmail.com>
2024-03-21 21:41:39 +0530

    Add cart API route implementation

---
+++ app/(rest)/cart/api/route.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const cookie = request.headers.get("Cookie")?.split("=")[1];
+  try {
+    const res = await axios.get(`${process.env.BASE_URL}/cart`, {
+      headers: {
+        Authorization: `Bearer ${cookie}`,
+      },
+    });
+    return new Response(JSON.stringify(res.data), {
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        return new Response(JSON.stringify(error.response.data), {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          status: 401,
+        });
+      }
+    }
+
+    return new Response(JSON.stringify({ message: "Something went wrong" }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 400,
+    });
+  }
+}

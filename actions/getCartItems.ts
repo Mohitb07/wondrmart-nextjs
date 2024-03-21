37ab6f898c81d76949a17e3c75f1f367708b4fc5
Mohitb07 <bmohit980@gmail.com>
2024-03-21 21:41:27 +0530

    refactor: to use api route and handle error response

--- actions/getCartItems.ts
+++ actions/getCartItems.ts
@@ -1,12 +1,25 @@
 import { CartItemData } from "@/types";
-import { axiosInstance } from "../api";
+import axios from "axios";
 
 export const getCartItems = async (): Promise<CartItemData> => {
   try {
-    const res = await axiosInstance.get("/cart");
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/cart/api`,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return res.data;
   } catch (error) {
     console.error("Error fetching cart items", error);
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        window.location.href = "/login";
+      }
+    }
     throw error;
   }
 };

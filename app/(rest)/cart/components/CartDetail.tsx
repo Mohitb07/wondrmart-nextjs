94dc49ecacad952f3a9505b5a7e24c63213de83b
mohitb07 <bmohit980@gmail.com>
2024-08-06 22:19:00 +0530

    refactor: update CartDetail component to use useGetCart custom hook

--- app/(rest)/cart/components/CartDetail.tsx
+++ app/(rest)/cart/components/CartDetail.tsx
@@ -14,17 +14,20 @@ import useGetUser from "@/hooks/useGetUser";
 import useRemoveCartItem from "@/hooks/useDeleteCartItem";
 import Image from "next/image";
 import Link from "next/link";
+import useGetCart from "@/hooks/useGetCart";
 
 const CartDetail = () => {
   const { data: user, isLoading: isUserLoading } = useGetUser();
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["cartItems"],
-    queryFn: getCartItems,
-  });
+  const {
+    data: cart,
+    isInitialLoading: isCartLoading,
+    isError,
+    error,
+  } = useGetCart();
   const { mutate: updateQuantityHandler } = useUpdateQuantity();
   const { mutate: removeCartItemHandler } = useRemoveCartItem();
 
-  if (isLoading || isUserLoading) {
+  if (isCartLoading || isUserLoading) {
     return (
       <div>
         <div className="flex items-center justify-center h-[500px] ">
@@ -35,17 +38,17 @@ const CartDetail = () => {
   }
 
   if (isError) {
-    return <div>Error: {JSON.stringify(isError)}</div>;
+    return <div>Error: {JSON.stringify(error)}</div>;
   }
 
-  const cartItems = data?.cart[0]?.cart_items || [];
-  let totalCartSum = calculateCartPrice(cartItems);
+  const userCart = cart?.cart_items || [];
+  let totalCartSum = calculateCartPrice(userCart);
   return (
     <>
       <h1 className="text-2xl ml-2 md:ml-0 md:text-3xl font-bold my-2">
         {user?.username}&apos;s Cart
       </h1>
-      {data.cart.length === 0 ? (
+      {userCart.length === 0 ? (
         <div className="flex justify-center items-center h-[500px] flex-col">
           <Image
             src={NotFoundSVG}
@@ -63,16 +66,16 @@ const CartDetail = () => {
           {/* left section */}
           <div className="lg:col-span-2">
             <div className="flex flex-col gap-3">
-              {cartItems.map((item) => (
+              {userCart.map((item) => (
                 <CartItem
                   onUpdateQuantity={updateQuantityHandler}
                   onRemoveCartItem={removeCartItemHandler}
                   key={item.cart_item_id}
-                  image_url={item.products.image_url}
+                  image_url={item.product.image_url}
                   product_id={item.product_id}
                   quantity={item.quantity}
-                  title={item.products.name}
-                  unit_amount={item.products.price}
+                  title={item.product.name}
+                  unit_amount={item.product.price}
                   total_amount={item.total_amount}
                   cart_id={item.cart_id}
                   cart_item_id={item.cart_item_id}

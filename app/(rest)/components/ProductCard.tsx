912c6cddea0a97bf0dde705eca44ab725fa6182d
mohitb07 <bmohit980@gmail.com>
2024-08-23 18:17:33 +0530

    fix: loading state added

--- app/(rest)/components/ProductCard.tsx
+++ app/(rest)/components/ProductCard.tsx
@@ -88,95 +88,95 @@ const ProductCard = ({
   };
 
   return (
-    // <Skeleton
-    //   isLoaded={!isLoading}
-    //   className="card max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
-    // >
-    <div
-      data-testid="product-card"
-      // className="max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
-      // className="card max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
+    <Skeleton
+      isLoaded={!isLoading}
+      className="card max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
     >
-      <Link href={`/product/${id}`}>
-        <div className="overflow-hidden h-[12rem] flex justify-center items-center bg-white">
-          <AdvancedImage
-            cldImg={productImage}
-            // plugins={[placeholder({ mode: "blur" })]}
-          />
-        </div>
-      </Link>
-      <div className="px-2 py-1 md:p-3 flex-1 flex flex-col justify-between">
+      <div
+        data-testid="product-card"
+        // className="max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
+        // className="card max-h-[25rem] max-w-[13rem] md:w-[13rem] rounded-md border border-slate-700 overflow-hidden flex flex-col max-sm:pb-2"
+      >
         <Link href={`/product/${id}`}>
-          <div>
-            <h2 className="line-clamp-1 text-sm">{name}</h2>
+          <div className="overflow-hidden h-[12rem] flex justify-center items-center bg-white">
+            <AdvancedImage
+              cldImg={productImage}
+              // plugins={[placeholder({ mode: "blur" })]}
+            />
           </div>
         </Link>
-        <div className="flex items-center justify-between mt-[1rem] flex-wrap">
-          <div>
-            <h2 className="text-sm">{formattedPrice}</h2>
-          </div>
-          <div>
-            <div className="flex items-center justify-end text-[1rem] max-sm:text-sm gap-2 w-[6rem]">
-              {/* <button className="bg-[#BEF264] text-black font-bold rounded-tl-xl rounded-bl-xl px-3 py-1">
+        <div className="px-2 py-1 md:p-3 flex-1 flex flex-col justify-between">
+          <Link href={`/product/${id}`}>
+            <div>
+              <h2 className="line-clamp-1 text-sm">{name}</h2>
+            </div>
+          </Link>
+          <div className="flex items-center justify-between mt-[1rem] flex-wrap">
+            <div>
+              <h2 className="text-sm">{formattedPrice}</h2>
+            </div>
+            <div>
+              <div className="flex items-center justify-end text-[1rem] max-sm:text-sm gap-2 w-[6rem]">
+                {/* <button className="bg-[#BEF264] text-black font-bold rounded-tl-xl rounded-bl-xl px-3 py-1">
                 -
               </button> */}
-              {/* <button disabled className="bg-[#BEF264] text-black font-bold py-1 w-6 text-center">
+                {/* <button disabled className="bg-[#BEF264] text-black font-bold py-1 w-6 text-center">
                 1
               </button>
               <button className=" bg-[#BEF264] rounded-tr-xl rounded-br-xl px-3 text-black font-bold py-1 ">
                 +
               </button> */}
-              {formattedCartQty > 0 ? (
-                <div className="w-full flex justify-between items-center">
-                  <Button
-                    isIconOnly
-                    size="sm"
-                    color="primary"
-                    variant="faded"
-                    aria-label="increase quantity"
-                    style={{
-                      fontSize: "1.2rem",
-                    }}
-                    onClick={onRemoveQty}
-                  >
-                    -
-                  </Button>
-                  <span>{formattedCartQty}</span>
-                  <Button
-                    isIconOnly
-                    size="sm"
-                    color="primary"
-                    variant="faded"
-                    aria-label="increase quantity"
-                    style={{
-                      fontSize: "1.2rem",
-                    }}
-                    onClick={onAddQty}
-                  >
-                    +
-                  </Button>
-                </div>
-              ) : (
-                <form onSubmit={handleAddToCart} className="w-full">
-                  <Button
-                    type="submit"
-                    fullWidth
-                    size="sm"
-                    color="primary"
-                    variant="faded"
-                    isLoading={isAdding}
-                    isDisabled={isAdding}
-                  >
-                    Add
-                  </Button>
-                </form>
-              )}
+                {formattedCartQty > 0 ? (
+                  <div className="w-full flex justify-between items-center">
+                    <Button
+                      isIconOnly
+                      size="sm"
+                      color="primary"
+                      variant="faded"
+                      aria-label="increase quantity"
+                      style={{
+                        fontSize: "1.2rem",
+                      }}
+                      onClick={onRemoveQty}
+                    >
+                      -
+                    </Button>
+                    <span>{formattedCartQty}</span>
+                    <Button
+                      isIconOnly
+                      size="sm"
+                      color="primary"
+                      variant="faded"
+                      aria-label="increase quantity"
+                      style={{
+                        fontSize: "1.2rem",
+                      }}
+                      onClick={onAddQty}
+                    >
+                      +
+                    </Button>
+                  </div>
+                ) : (
+                  <form onSubmit={handleAddToCart} className="w-full">
+                    <Button
+                      type="submit"
+                      fullWidth
+                      size="sm"
+                      color="primary"
+                      variant="faded"
+                      isLoading={isAdding}
+                      isDisabled={isAdding}
+                    >
+                      Add
+                    </Button>
+                  </form>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    // </Skeleton>
+    </Skeleton>
   );
 };
 export default ProductCard;

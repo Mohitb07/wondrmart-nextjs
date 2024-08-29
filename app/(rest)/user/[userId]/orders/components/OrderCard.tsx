621ca9b81b9a8297bda6269df670b4b38e2cd8ba
mohitb07 <bmohit980@gmail.com>
2024-08-29 21:54:44 +0530

    refactor: to display order details and improve UI

--- app/(rest)/user/[userId]/orders/components/OrderCard.tsx
+++ app/(rest)/user/[userId]/orders/components/OrderCard.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { cloudinaryImage } from "@/utils/cloudinaryImage";
+import { formatPrice } from "@/utils/formatPrice";
+import { AdvancedImage } from "@cloudinary/react";
 import { Button, Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
-import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { FaChevronRight } from "react-icons/fa6";
@@ -9,87 +11,102 @@ import { FaChevronRight } from "react-icons/fa6";
 type OrderCardProps = {
   orderId: string;
   isOrderDetail?: boolean;
+  orderDate: string;
+  orderAmount: string;
+  productImage: string;
+  productName: string;
+  username: string;
 };
 
 const OrderCard: React.FC<OrderCardProps> = ({
   orderId = "",
   isOrderDetail = false,
+  orderDate,
+  orderAmount,
+  username,
+  productImage,
+  productName,
 }) => {
   const router = useRouter();
   const pathname = usePathname();
+  const orderPlacedOn = new Date(orderDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  console.log(orderPlacedOn); // Output: Aug 28, 2024
+
+  const totalAmount = formatPrice(Number(orderAmount));
+  const productShowCaseImage = cloudinaryImage({
+    imageUrl: productImage,
+    height: 100,
+    width: 100,
+  });
 
   const handleNavigate = () => {
     router.push(`${pathname}/${orderId}`);
   };
 
   return (
-    <>
-      <Card
-        className="shadow-2xl shadow-gray-900 w-full md:border border-gray-800"
-        isBlurred
-      >
-        <CardHeader className="hidden md:block">
-          <div className="flex items-center justify-between w-full text-slate-400">
-            <div className="flex items-center justify-center text-xs gap-10 text-left">
-              <div>
-                <h1 className="uppercase">Order Placed</h1>
-                <h1>29 August 2019</h1>
-              </div>
-              <div>
-                <h1 className="uppercase">Total</h1>
-                <h1>21000.00</h1>
-              </div>
-              {!isOrderDetail && (
-                <div>
-                  <h1 className="uppercase">Ship To</h1>
-                  <h1>John Doe</h1>
-                </div>
-              )}
+    <Card
+      className="shadow-2xl shadow-gray-900 w-full md:border border-gray-800"
+      isBlurred
+    >
+      <CardHeader className="hidden md:block">
+        <div className="flex items-center justify-between w-full text-slate-400">
+          <div className="flex items-center justify-center text-xs gap-10 text-left">
+            <div>
+              <h1 className="uppercase">Order Placed</h1>
+              <h1>{orderPlacedOn}</h1>
             </div>
-            <div className="text-left text-xs">
-              <h1 className="uppercase">Order #1234331232</h1>
-            </div>
-          </div>
-        </CardHeader>
-        <Divider className="hidden md:block" />
-        <CardBody className="p-1 md:p-3">
-          <div className="flex items-center justify-around md:justify-between">
-            <div className="flex items-center justify-between">
-              <Image
-                className="rounded-lg"
-                src="https://m.media-amazon.com/images/I/61SUj2aKoEL._SX679_.jpg"
-                alt="product image"
-                width={100}
-                height={100}
-                priority
-              />
-              <div className="p-4 text-left">
-                <h1 className="text-lg font-bold line-clamp-2">
-                  Apple AirPods Pro (2nd Generation)
-                </h1>
-                <p className="text-slate-400 text-sm">Ordered on 12/12/2021</p>
-              </div>
+            <div>
+              <h1 className="uppercase">Total</h1>
+              <h1>{totalAmount}</h1>
             </div>
             {!isOrderDetail && (
-              <div className="md:hidden">
-                <FaChevronRight className="text-slate-400" />
-              </div>
-            )}
-            {!isOrderDetail && (
-              <div className="md:block hidden">
-                <Button
-                  variant="solid"
-                  color="primary"
-                  onClick={handleNavigate}
-                >
-                  Order Detail
-                </Button>
+              <div>
+                <h1 className="uppercase">Ship To</h1>
+                <h1>{username}</h1>
               </div>
             )}
           </div>
-        </CardBody>
-      </Card>
-    </>
+          <div className="text-left text-xs">
+            <h1 className="uppercase">Order #{orderId}</h1>
+          </div>
+        </div>
+      </CardHeader>
+      <Divider className="hidden md:block" />
+      <CardBody className="p-1 md:p-3">
+        <div className="flex items-center justify-around md:justify-between">
+          <div className="flex items-center justify-between">
+            <AdvancedImage
+              cldImg={productShowCaseImage}
+              // plugins={[placeholder({ mode: "blur" })]}
+            />
+            <div className="p-4 text-left space-y-2">
+              <h1 className="text-base font-bold line-clamp-2">
+                {productName}
+              </h1>
+              <p className="text-slate-400 text-xs block md:hidden">
+                Ordered on {orderPlacedOn}
+              </p>
+            </div>
+          </div>
+          {!isOrderDetail && (
+            <div className="md:hidden">
+              <FaChevronRight className="text-slate-400" />
+            </div>
+          )}
+          {!isOrderDetail && (
+            <div className="md:block hidden">
+              <Button variant="solid" color="primary" onClick={handleNavigate}>
+                Order Detail
+              </Button>
+            </div>
+          )}
+        </div>
+      </CardBody>
+    </Card>
   );
 };
 export default OrderCard;

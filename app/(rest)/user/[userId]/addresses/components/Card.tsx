90d113cf1b4ef0cb279e9c68c56b9262ea13ca1e
mohitb07 <bmohit980@gmail.com>
2024-09-04 21:58:21 +0530

    feat: update with order address

--- app/(rest)/user/[userId]/addresses/components/Card.tsx
+++ app/(rest)/user/[userId]/addresses/components/Card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { REGIONS_COUNTRIES } from "@/constants";
 import {
   Card,
   CardBody,
@@ -22,9 +23,11 @@ type CardProps = {
   area?: string;
   city?: string;
   state?: string;
+  street?: string;
   country?: string;
   mobile?: string;
   addressId?: string;
+  pincode?: string;
 };
 
 const StyledCard: React.FC<CardProps> = ({
@@ -38,38 +41,58 @@ const StyledCard: React.FC<CardProps> = ({
   area = "",
   city = "",
   state = "",
+  street = "",
   country = "",
   mobile = "",
+  pincode = "",
   addressId,
 }) => {
+  const region = REGIONS_COUNTRIES.map((region) => {
+    if (region.countryShortCode === country) {
+      let countryName = region.countryName;
+      let stateName = null;
+      region.regions.map(
+        (reg) => reg.shortCode === state && (stateName = reg.name)
+      );
+      return {
+        country: countryName,
+        state: stateName,
+      };
+    }
+  });
+  const result = region.filter((reg) => Boolean(reg));
+  const { country: countryName, state: stateName } = result[0] || {};
   return (
     <Card
-      className={`w-full md:min-w-[300px] md:max-w-[380px] min-h-[240px] border ${
+      className={`w-full md:min-w-[300px] md:max-w-[380px] h-full border ${
         isDefault ? "border-primary" : "border-transparent"
       } ${className}`}
     >
       {isHeaderVisible && isDefault && (
         <>
-          <CardHeader className="flex gap-3 text-xs p-3 px-6">
+          <CardHeader className="flex gap-3 text-xs p-3">
             <p>Default: </p>
             <span>wondrMart</span>
           </CardHeader>
           <Divider />
         </>
       )}
-      <CardBody className="text-sm p-3 px-6 flex justify-center">
-        {children || (
-          <div className="leading-5 flex flex-col">
+      {children || (
+        <CardBody className="p-3 text-sm flex justify-center">
+          <div className="flex flex-col">
             <span>{apartment}</span>
+            <span>{street}</span>
             <span>{area}</span>
             <span>{city}</span>
-            <span>{state}</span>
-            <span>Phone number: {mobile}</span>
+            <span>{pincode}</span>
+            <span>Ph: {mobile}</span>
+            <span>{stateName}</span>
+            <span>{countryName}</span>
           </div>
-        )}
-      </CardBody>
-      <CardFooter className="flex text-sm gap-3 px-6">
-        {isFooterVisible && !children && (
+        </CardBody>
+      )}
+      {isFooterVisible && !children && (
+        <CardFooter className="flex text-sm gap-3 px-6">
           <>
             <div>
               <Link
@@ -91,8 +114,8 @@ const StyledCard: React.FC<CardProps> = ({
               </>
             )}
           </>
-        )}
-      </CardFooter>
+        </CardFooter>
+      )}
     </Card>
   );
 };

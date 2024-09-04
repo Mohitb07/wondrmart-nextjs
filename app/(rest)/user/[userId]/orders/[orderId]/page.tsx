46875ae76759f000ff701ca10c8bf634d5c66005
mohitb07 <bmohit980@gmail.com>
2024-09-04 21:54:24 +0530

    feat: fetch and display user's order details

--- app/(rest)/user/[userId]/orders/[orderId]/page.tsx
+++ app/(rest)/user/[userId]/orders/[orderId]/page.tsx
@@ -1,6 +1,7 @@
 import OrderSummary from "@/app/(rest)/cart/components/Summary";
 import StyledCard from "../../addresses/components/Card";
 import OrderCard from "../components/OrderCard";
+import { getUserOrder } from "@/actions/getOrder";
 
 export const metadata = {
   title: "Order Detail",
@@ -9,34 +10,58 @@ export const metadata = {
 
 type OrderDetailProps = {
   params: {
+    userId: string;
     orderId: string;
   };
 };
 
-export default function OrderDetail({ params }: OrderDetailProps) {
+export default async function OrderDetail({ params }: OrderDetailProps) {
+  const { userId, orderId } = params;
+  const orderDetail = (await getUserOrder(userId, orderId)) || {};
+
+  console.log("orderDetail", orderDetail);
+  const orderItems = orderDetail.order_items || [];
   return (
-    <div className="">
-      <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-10 w-full ">
-        <div className="space-y-3  h-full">
+    <div>
+      <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-10 w-full items-stretch py-2">
+        <div className="space-y-3 h-full">
           <h2 className="text-xl font-bold">Shipped To</h2>
-          <StyledCard isFooterVisible={false} isHeaderVisible={false} />
+          <StyledCard
+            userId={orderDetail.customer_id}
+            apartment={orderDetail.address.flat_no}
+            street={orderDetail.address.street}
+            country={orderDetail.address.country}
+            city={orderDetail.address.city}
+            state={orderDetail.address.state}
+            mobile={orderDetail.address.phone}
+            pincode={orderDetail.address.pincode}
+            isFooterVisible={false}
+            isHeaderVisible={false}
+          />
         </div>
         <div className="space-y-3 h-full">
           <h2 className="text-xl font-bold">Order Summary</h2>
-          <OrderSummary textSize="sm" amountSize="base" totalAmount={0}/>
+          <OrderSummary
+            textSize="sm"
+            amountSize="base"
+            totalAmount={orderDetail.order_amount}
+            quantity={orderDetail.order_items.length}
+          />
         </div>
       </div>
       <div className="mt-5 space-y-5">
         {/* NEED TO IMPLEMENT THIS */}
-        {/* <OrderCard orderId="313" isOrderDetail />
-        <OrderCard orderId="313312" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail />
-        <OrderCard orderId="31351" isOrderDetail /> */}
+        {orderItems.map((orderItem) => (
+          <OrderCard
+            orderId="313"
+            isOrderDetail
+            orderAmount={orderItem.total_amount}
+            orderDate={orderItem.createdAt}
+            productImage={orderItem.product.image_url}
+            productName={orderItem.product.name}
+            username={orderDetail.address.customer.username}
+          />
+        ))}
       </div>
     </div>
   );

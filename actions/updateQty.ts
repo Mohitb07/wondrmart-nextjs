10559b81977d50f9ea3b313f0abb971ad0317e3a
mohitb07 <bmohit980@gmail.com>
2024-09-04 21:52:21 +0530

    refactor: Add error handling

--- actions/updateQty.ts
+++ actions/updateQty.ts
@@ -4,13 +4,17 @@ export const addQuantity = async (
   productId: string,
   productPrice: string,
   quantity: string,
-  cartId: string,
+  cartId: string
 ) => {
-  const res = await axiosInstance.patch("/carts", {
-    product_id: productId,
-    product_price: productPrice,
-    quantity,
-    cart_id: cartId,
-  });
-  return res.data;
+  try {
+    const res = await axiosInstance.patch("/carts", {
+      product_id: productId,
+      product_price: productPrice,
+      quantity,
+      cart_id: cartId,
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
 };

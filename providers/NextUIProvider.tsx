2044e664473bde50708682778dbcf573feaa50d5
mohitb07 <bmohit980@gmail.com>
2024-10-15 11:55:52 +0530

    refactor: remove NextTopLoader

--- providers/NextUIProvider.tsx
+++ providers/NextUIProvider.tsx
@@ -2,16 +2,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import NextTopLoader from "nextjs-toploader";
 
 export function UIProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      <NextTopLoader
-        color="#BEF264"
-        showSpinner={false}
-      />
-      {children}
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }

75dbb0622ca0353329fc94d6f98599feb3d128ee
mohitb07 <bmohit980@gmail.com>
2024-10-15 12:13:49 +0530

    Add notification component

---
+++ common/Notification/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import toast, { ToastBar, Toaster } from "react-hot-toast";
+import { HiX } from "react-icons/hi";
+
+const CustomNotification = () => {
+  return (
+    <div>
+      <Toaster
+        reverseOrder={false}
+        position="top-right"
+        toastOptions={{
+          style: {
+            borderRadius: "8px",
+            background: "#232531",
+            // color: "#fff",
+          },
+        }}
+      >
+        {(t) => (
+          <ToastBar toast={t}>
+            {({ icon, message }) => (
+              <>
+                <div className="flex flex-col gap-2 text-[10px] sm:text-xs z-50">
+                  <div className="error-alert cursor-default flex items-center justify-between w-full rounded-lg bg-[#232531] px-[10px]">
+                    <div className="flex gap-2 items-center">
+                      {icon}
+                      <div>
+                        <p className="text-white text-base">Please try again</p>
+                        <p className="text-gray-500 text-sm -ml-3">{message}</p>
+                      </div>
+                    </div>
+                    {t.type !== "loading" && (
+                      <button
+                        className="rounded-full p-1 ring-primary-400 transition hover:bg-[#444] focus:outline-none focus-visible:ring"
+                        onClick={() => toast.dismiss(t.id)}
+                      >
+                        <HiX />
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+    </div>
+  );
+};
+
+export default CustomNotification;

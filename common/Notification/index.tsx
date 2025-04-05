bb7b971a6411278b408b4f9a7447bd22cc5d9471
mohitb07 <bmohit980@gmail.com>
2025-04-05 13:44:35 +0530

    refactor: simplify CustomNotification component structure and improve styling

--- common/Notification/index.tsx
+++ common/Notification/index.tsx
@@ -5,47 +5,44 @@ import { HiX } from "react-icons/hi";
 
 const CustomNotification = () => {
   return (
-    <div>
-      <Toaster
-        reverseOrder={false}
-        position="top-right"
-        toastOptions={{
-          style: {
-            borderRadius: "8px",
-            background: "#232531",
-            // color: "#fff",
-          },
-        }}
-      >
-        {(t) => (
-          <ToastBar toast={t}>
-            {({ icon, message }) => (
-              <>
-                <div className="flex flex-col gap-2 text-[10px] sm:text-xs z-50">
-                  <div className="error-alert cursor-default flex items-center justify-between w-full rounded-lg bg-[#232531] px-[10px]">
-                    <div className="flex gap-2 items-center">
-                      {icon}
-                      <div>
-                        <p className="text-white text-base">Please try again</p>
-                        <p className="text-gray-500 text-sm -ml-3">{message}</p>
-                      </div>
+    <Toaster
+      reverseOrder={false}
+      position="top-right"
+      toastOptions={{
+        style: {
+          borderRadius: "8px",
+          background: "#232531",
+        },
+      }}
+    >
+      {(t) => (
+        <ToastBar toast={t}>
+          {({ icon, message }) => (
+            <>
+              <div className="flex flex-col gap-2 text-[10px] sm:text-xs z-50">
+                <div className="error-alert cursor-default flex items-center justify-between w-full rounded-lg  px-[10px]">
+                  <div className="flex gap-4 items-center">
+                    {icon}
+                    <div>
+                      <p className="text-white text-base">Try Again</p>
+                      <p className="text-gray-400 text-sm -ml-3">{message}</p>
                     </div>
-                    {t.type !== "loading" && (
-                      <button
-                        className="rounded-full p-1 ring-primary-400 transition hover:bg-[#444] focus:outline-none focus-visible:ring"
-                        onClick={() => toast.dismiss(t.id)}
-                      >
-                        <HiX />
-                      </button>
-                    )}
                   </div>
+                  {t.type !== "loading" && (
+                    <button
+                      className="rounded-full p-1 ring-primary-400 transition text-gray-400 hover:bg-[#444] focus:outline-none focus-visible:ring"
+                      onClick={() => toast.dismiss(t.id)}
+                    >
+                      <HiX />
+                    </button>
+                  )}
                 </div>
-              </>
-            )}
-          </ToastBar>
-        )}
-      </Toaster>
-    </div>
+              </div>
+            </>
+          )}
+        </ToastBar>
+      )}
+    </Toaster>
   );
 };
 

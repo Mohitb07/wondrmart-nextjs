d3637ebf797aa00ffe3c9539dbe3b8eb7a6be40a
mohitb07 <bmohit980@gmail.com>
2025-12-04 15:53:34 +0530

    feat: added user retrieval by id function

--- actions/getUser.ts
+++ actions/getUser.ts
@@ -1,5 +1,6 @@
-import { axiosInstance } from "@/api";
+import { axiosInstance, BASE_URL } from "@/api";
 import { User } from "@/types";
+import axios from "axios";
 import Cookies from "js-cookie";
 
 export const getUser = async (): Promise<User> => {
@@ -11,6 +12,21 @@ export const getUser = async (): Promise<User> => {
     const res = await axiosInstance.get("/me");
     return res.data;
   } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserById = async (userId: string): Promise<User | null> => {
+  if (!userId) {
+    throw new Error("No userId provided");
+  }
+  try {
+    const res = await axios.get(BASE_URL + `/get_user_by_id/${userId}`);
+    return res.data;
+  } catch (error: any) {
+    if (error.response?.status === 404) {
+      return null; // user doesn't exist
+    }
     throw error;
   }
 };

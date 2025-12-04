48b2697bbb46e94419c4928f2605f12d8b66aa6e
mohitb07 <bmohit980@gmail.com>
2025-12-04 15:54:54 +0530

    fix: ensure SignUp component has consistent width styling

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function SignUp() {
   return (
     <div className="w-full min-h-[calc(100vh-5rem)] flex items-center justify-center">
-      <div className="px-4  py-16 sm:px-6 lg:px-8 space-y-5 min-w-[20rem] md:min-w-[30rem]">
+      <div className="px-4  py-16 sm:px-6 lg:px-8 space-y-5 min-w-[20rem] md:min-w-[30rem] w-full md:w-fit">
         <h1 className="text-4xl font-bold text-center">Sign Up</h1>
         <SignUpBody />
       </div>

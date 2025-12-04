2989eeff397e9b9bfdd5c6f670a0790cc8f61176
mohitb07 <bmohit980@gmail.com>
2025-12-04 15:52:00 +0530

    refactor: files moved

--- utils/formatPrice.ts
+++
@@ -1,9 +0,0 @@
-export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency: "INR",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
-  return formatter.format(price);
-};

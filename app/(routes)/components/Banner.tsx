430930171bc7a1b405e69042959233163249e949
mohitb07 <bmohit980@gmail.com>
2026-08-12 13:57:55 +0530

    fix: improved loading speed

--- app/(routes)/components/Banner.tsx
+++ app/(routes)/components/Banner.tsx
@@ -1,44 +1,24 @@
-"use client";
-
 import DesktopBanner from "@/public/images/banner-desktop.png";
 import MobileBanner from "@/public/images/banner-mobile.jpg";
-import Image, { StaticImageData } from "next/image";
-import { useEffect, useState } from "react";
-
-const [smallerBanner, largeBanner] = [MobileBanner, DesktopBanner];
+import Image from "next/image";
 
 const Banner = () => {
-  const [banner, setBanner] = useState<StaticImageData | null>(null); // Use null instead of undefined
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    function handleResize() {
-      const isSmaller = window.innerWidth < 768;
-      setBanner(isSmaller ? smallerBanner : largeBanner);
-      setIsLoading(false);
-    }
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="banner-container">
-      {isLoading
-        ? null
-        : banner && ( // Ensure banner is not null before rendering Image
-            <Image
-              src={banner}
-              width={0}
-              height={0}
-              decoding="async"
-              priority={true}
-              alt="recently launched product banner"
-              className="banner-image"
-            />
-          )}
+      <Image
+        src={MobileBanner}
+        alt="recently launched product banner"
+        className="banner-image block md:hidden"
+        priority
+        sizes="100vw"
+      />
+      <Image
+        src={DesktopBanner}
+        alt="recently launched product banner"
+        className="banner-image hidden md:block"
+        priority
+        sizes="100vw"
+      />
     </div>
   );
 };

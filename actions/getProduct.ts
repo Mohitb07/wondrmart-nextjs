16296d7351f9fe178d8d45d0c697e099fa948f66
mohitb07 <bmohit980@gmail.com>
2026-08-12 12:54:47 +0530

    fix: product detail not found issue

--- actions/getProduct.ts
+++ actions/getProduct.ts
@@ -4,7 +4,11 @@ import axios from "axios";
 
 export const getProduct = async (productId: string): Promise<Product> => {
   try {
-    const res = await axios.get(BASE_URL + `/products/${productId}`)
+    const baseUrl =
+      typeof window === "undefined"
+        ? process.env.BASE_URL || process.env.NEXT_PUBLIC_BASE_URL
+        : BASE_URL;
+    const res = await axios.get(`${baseUrl}/products/${productId}`);
     return res.data;
   } catch (error) {
     throw error;

8921792bf8cf0d720e5514e0ee792ceb1d66f10d
mohitb07 <bmohit980@gmail.com>
2026-08-20 18:25:31 +0530

    fix: wrong api url

--- actions/getOrderStatus.ts
+++ actions/getOrderStatus.ts
@@ -1,11 +1,12 @@
-import { axiosInstance, BASE_URL } from "@/api";
+import { axiosInstance } from "@/api";
 import { Order } from "@/types";
 
 export const getOrderStatus = async (orderId: string): Promise<Order> => {
   try {
-    const res = await axiosInstance.get(BASE_URL + `/verify_order/${orderId}`);
+    const res = await axiosInstance.get(`/verify_order/${orderId}`);
     return res.data;
   } catch (error) {
     throw error;
   }
 };
+
